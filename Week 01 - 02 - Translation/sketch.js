32a45eefb53addd719cd34101cc702c27d2c7123
Kittiat Bamrung <n0020132@iadt.ie>
2022-02-01 19:00:01 +0000

    added translation

--- Week 01 - 02 - Translation/sketch.js
+++ Week 01 - 02 - Translation/sketch.js
@@ -6,13 +6,12 @@ function setup() {
     createCanvas(500,500);
     background(120);
     rectMode(CENTER);
-    angleMode(DEGREE);
-    if (showGrid == true) {
-        grid(20, 20);
-    }
+    angleMode(DEGREES);
 }
 
 function draw() {
-    fill(255, 0, 0);
-    rect(100, 100, 50, 50);
+    background(120);
+    if (showGrid == true) {
+        grid(20, 20);
+    }
 }
--- Week 01 - 02 - drawGrid/function.js
+++ Week 01 - 02 - drawGrid/function.js
@@ -1,8 +1,8 @@
 function grid(numGridX, numGridY) {
     let amountX = width / numGridX;
     let amountY = height / numGridY;
-    for (let j = 0; j < numGridX; j++) {
-        for (let i = 0; i < numGridY; i++) {
+    for (let j = 0; j < numGridY; j++) {
+        for (let i = 0; i < numGridX; i++) {
             noFill();
             stroke(255, 20);
             rect(i * amountX, j * amountY, amountX, amountY);
